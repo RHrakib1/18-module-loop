515bcde94c7a36c255b327ab681ea37532c96f88
RHrakib1 <rakibhasanmd457@gmail.com>
2024-01-31 19:32:59 +0600

    oush the deferance ways in loop odd,even numbers

---
+++ deferance-odd-even.js
@@ -0,0 +1,38 @@
+// ----------odd number ber korar simple niyom-------------
+console.log("simple niyome odd")
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 1) {
+        console.log(i)
+    }
+}
+
+
+// vinno niyome odd ber kora
+console.log("odd deferance niyom a ");
+for (let i = 1; i < 30; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+
+
+
+// simple niyome even number
+console.log("example simple even number");
+for (let i = 10; i <= 50; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+
+
+// vinno niyome even number bar korar niyom
+console. log("example onno niyome even bar korar niyom");
+for (i = 20; i <= 30; i++) {
+    if (i % 2 !== 1){
+        console.log(i);
+    }
+
+}
