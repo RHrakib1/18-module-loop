253840898f12d0dd95df7ec0043bea71cdec1f36
RHrakib1 <rakibhasanmd457@gmail.com>
2024-01-31 18:41:01 +0600

    added to the incriment and decriment

--- usefor.js
+++ usefor.js
@@ -9,9 +9,22 @@ console.log("example 3 ")
 // example for loop with sum
 let sum = 0;
 for (let i = 11; i <= 20; i++) {
-    
+
     sum = sum + i;
 }
 console.log("sum of number :", sum);
 
 
+
+// decriment
+console.log("decriment");
+for (let i = 20; i >= 11; i--) {
+    console.log(i);
+
+}
+
+// example incriment
+console.log("incriment")
+for(i=1; i<=30; i++){
+    console.log(i);
+}
