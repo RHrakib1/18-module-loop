9eaef085d87e5745130a15d4a92286d539879421
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-02 14:45:47 +0600

    1

--- for.js
+++ for.js
@@ -22,3 +22,62 @@ for (let num = 1; num <= 50; num++) {
         console.log("even number:", num);
     }
 }
+
+
+
+console.log('array er sahajje for loop bebohar');
+let component = ['cpu', 'ram', 'memory', 'timer', 'motherbord'];
+for (let i = 0; i < component.length; i++) {
+    console.log(component[2]);
+}
+
+
+
+
+
+
+
+
+console.log('array er sahajje for loop bebohar');
+
+let raa = [10, 20, 30, 40, 50, 60];
+let oddOfSum = 0;
+let evenOfSum = 0;
+
+for (let i = 0; i < raa.length; i++) {
+    const x = raa[i];
+    if (x % 2 == 0) {
+        oddOfSum = oddOfSum + i;
+    }
+    else {
+        evenOfSum = evenOfSum + i;
+    }
+}
+console.log(oddOfSum);
+console.log(evenOfSum);
+
+
+
+
+
+
+
+
+
+
+
+conosle.log("exampple for loop in array to the sum")
+numbert = [11, 55, 48, 64, 20, 125, 111, 80];
+let sumofOdd = 0;//11, 55, 125, 111
+let sumofEven = 0;//48, 64, 20, 80
+for (let i = 0; i < numbert.length; i++) {
+    const x = numbert[i];
+    if (x % 2 === 0) {
+        sumofEven += x;
+    }
+    else {
+        sumofOdd += x;
+    }
+}
+console.log(sumofEven);
+console.log(sumofOdd);
---
+++ forOf.js
@@ -0,0 +1,14 @@
+// for loop
+let array1=[1, 21, 522, 80, 145, 50];
+for(let i=0; i<array1.length; i++){
+    console.log(array1[i]);
+}
+
+
+
+console.log("example of forof loop");
+let raad=[222, 50, 702, 955, 816, 1023];
+for(let rakib of raad){
+    console.log(rakib);
+}
+
